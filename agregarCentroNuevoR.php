db62c9e7251179d776d8ced0f9d82af57bfa8005
sikeze <mipozarski@alumnos.uai.cl>
2015-07-23 19:11:15 -0300

    Cambio agregar Centro
    se modifico query para agregar ademas del centro el nombre de las Ecos y
    se agrego la opcion para que al agregar centro se pueda ingresar el
    numero de Ecos de este

--- agregarCentroNuevoR.php
+++ agregarCentroNuevoR.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+session_start ();
 include "header.php";
 include "include/verificacionUsuario.php";
 ?>
@@ -10,49 +10,55 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <html>
-    <head>
-       <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-    </head>
-    <body background="images/bg.gif">
-         <div class="container">
-      <h2>Agregar Centro Nuevo</h2>
-      <form action='querys/insertCentroNuevoR.php' method='POST'>
-        <div class="form-group">
-          <label for="nombre" >Nombre</label>
-          <input type="text" class="form-control" name="nombre" placeholder="Agrege nombre" required>
-        </div>
-         <div class="form-group">
-          <label for="siglas">Siglas</label>
-          <input type="text" class="form-control" name="siglas" placeholder="Agrege siglas" required>
-        </div>
-          </br>
-          
-      
-          
-          <label>Empresa a la que pertenece</label>
-          <br>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet"
+	href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+</head>
+<body background="images/bg.gif">
+	<div class="container">
+		<h2>Agregar Centro Nuevo</h2>
+		<form action='querys/insertCentroNuevoR.php' method='POST'>
+			<div class="form-group">
+				<label for="nombre">Nombre</label> <input type="text"
+					class="form-control" name="nombre" placeholder="Agrege nombre"
+					required>
+			</div>
+			<div class="form-group">
+				<label for="siglas">Siglas</label> <input type="text"
+					class="form-control" name="siglas" placeholder="Agrege siglas"
+					required>
+			</div>
+			<div class="form-group">
+				<label for="siglas">Numero de ecos</label> <input type="number"
+					class="form-control" name="ecos"
+					placeholder="Agrege numero de ecos" required>
+			</div>
+			<label>Empresa a la que pertenece</label> <br>
          
           <?php
-include_once "conexionLocal.php";
-  
-    $query = "SELECT * from Empresa order by Nombre asc ";
+										include_once "conexionLocal.php";
+										
+										$query = "SELECT * from Empresa order by Nombre asc ";
+										
+										$resultado = mysql_query ( $query ) or die ( mysql_error () );
+										if ($resultado) {
+											echo "<select required name='empresa' id='empresa' >";
+											echo "<option value=''> Seleccione Empresa </option>";
+											while ( $row = mysql_fetch_assoc ( $resultado ) ) {
+												
+												echo "<option value='" . $row ['idEmpresa'] . "'>" . $row ['Nombre'] . "</option>";
+											}
+											echo "</select><br>";
+											echo "<br>";
+											echo "<input class='btn btn-success' type='submit' value='Agregar'>";
+											
+											echo "</form>";
+										}
+										?>
+    
 
-    $resultado = mysql_query($query) or die(mysql_error());
-    if ($resultado) {
-        echo"<select required name='empresa' id='empresa' >";
- echo "<option value=''> Seleccione Empresa </option>";
-        while ($row = mysql_fetch_assoc($resultado)) {
 
-            echo "<option value='" . $row['idEmpresa'] . "'>" . $row['Nombre'] . "</option>";
-        }
-        echo"</select><br>";
-        echo "<br>";
-        echo"<input class='btn btn-success' type='submit' value='Agregar'>";
-        
-        echo"</form>";
-      
-    }
-      ?>
-    </body>
+
+</body>
 </html>
--- querys/insertCentroNuevoR.php
+++ querys/insertCentroNuevoR.php
@@ -1,32 +1,47 @@
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
+<head>
+<meta charset="UTF-8">
+<title></title>
+</head>
+<body>
         <?php
-         include_once "../conexionLocal.php";
-         $idEmpresa=$_POST['empresa'];
-          $nombre=$_POST['nombre'];
-           $siglas=$_POST['siglas'];
-          
-              
-              
-    $query="insert into Centro values (null,$idEmpresa,'$nombre','$siglas')";    
-    $resultado=mysql_query($query) ;
-    if($resultado) { 
-    //success 
-        echo"Agregado con exito, redireccionando";
-        ?><meta http-equiv="Refresh" content="1;url=../agregarCentroNuevoR.php">;
+								include_once "../conexionLocal.php";
+								$idEmpresa = $_POST ['empresa'];
+								$nombre = $_POST ['nombre'];
+								$siglas = $_POST ['siglas'];
+								$numeroecos = $_POST ['ecos'];
+								
+								$query = "insert into Centro values (null,$idEmpresa,'$nombre','$siglas')";
+								$resultado = mysql_query ( $query );
+								
+								$querybuscar = "SELECT idCentro FROM Centro WHERE Empresa_idEmpresa=$idEmpresa";
+								$resultadobuscar = mysql_query ( $querybuscar );
+								
+								$idcentro = mysql_fetch_assoc ( $resultadobuscar );
+								$valoridcentro = $idcentro ['idCentro'];
+								
+								for($contador = 1; $contador <= $numeroecos; $contador ++) {
+									$query2 = "insert into Ecos values (null,$valoridcentro,$contador)";
+									$resultado2 = mysql_query ( $query2 );
+								}
+								if ($resultado && $resultado2) {
+									// success
+									echo "Centro agregado con exito, redireccionando";
+									?>
+									<meta http-equiv="Refresh"
+		content="1;url=../agregarCentroNuevoR.php">;
         <?php
-} else { 
-    //failure
-    echo " El nombre o sigla ya existe, redireccionando";
-    ?>
+								} else {
+									// failure
+									echo " El nombre o sigla ya existe, redireccionando";
+									?>
         
-    <meta http-equiv="Refresh" content="1;url=../agregarCentroNuevoR.php">;
+    <meta http-equiv="Refresh"
+		content="1;url=../agregarCentroNuevoR.php">;
     <?php
-}        
-    ?>
+								}
+								
+								?>
+								
     </body>
 </html>
