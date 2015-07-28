17ef9d26d8d9a7208f1d9ea44ccbda755b624a3d
Cesar Gonzalez <cgonzb@gmail.com>
2015-07-28 17:30:31 -0300

    funcion de verificacion

--- Include/verificacionUsuario.php
+++ Include/verificacionUsuario.php
@@ -1,18 +1,14 @@
 <?php
+if (isset ( $_SESSION ["usuario"] )) {
+	
+	// osea si se loggeo
+	$nombreUsuario = $_SESSION ["usuario"];
+	$idUsuario = $_SESSION ["idusuario"];
+} 
 
-if(isset($_SESSION["usuario"]))
-{
-
-//osea si se loggeo
-$nombreUsuario=$_SESSION["usuario"];
-$idUsuario=$_SESSION["idusuario"];
-
-
+else {
+	header ( "location:logIn.php" );
+	$_SESSION ["fallaste"] = "Debes ingresar primero";
 }
 
-else{
-  header("location:logIn.php");
-$_SESSION["fallaste"]="Debes ingresar primero";
-}
- 
 ?>
--- querys/verify.php
+++ querys/verify.php
@@ -15,16 +15,20 @@ function verifyEco($idEco, $dateTime, $type = 'array', $display = false) {
 		$query = "SELECT TM_idTM FROM evento WHERE Ecos_idEcos = $idEco AND HoraInicio = '$dateTime' ";
 		$res = mysql_query ( $query ) or die ( mysql_error () ); // ejecutar la query
 		if (mysql_affected_rows () >= 1) { // si hay algun error
-			$result = array (
-					'error' => 'La Eco ya tiene asignado un TM' 
-			);
+			$result = false;
+		} else {
+			$result = true;
 		}
 	} else {
 		$query = "SELECT TM.Nombre FROM TM, evento WHERE Ecos_idEcos = $idEco AND HoraInicio = '$dateTime' AND TM_idTM = idTM";
 		$res = mysql_query ( $query ) or die ( mysql_error () ); // ejecutar la query
-		while ( $row = mysql_fetch_assoc ( $res ) ) {
-			$result [] = $row;
-		} // se guardan los valores en $result
+		if (mysql_affected_rows () >= 1) { // si hay algun error
+			while ( $row = mysql_fetch_assoc ( $res ) ) {
+				$result [] = $row;
+			} // se guardan los valores en $result
+		}else{
+			$result = true;
+		}
 	}
 	
 	if ($type = 'json') {
@@ -34,6 +38,37 @@ function verifyEco($idEco, $dateTime, $type = 'array', $display = false) {
 	}
 }
 
-echo verifyEco(1, '2015-07-27 09:00:00');//coincidencia de evento sin display
+// verificacion del tipo 2
+function verifyTM($idTM, $dateTime, $type = 'array', $display = false) {
+	if ($display == false) { // si solo se busca verificar (sin mostrar los duplicados)
+		$query = "SELECT Ecos_idEcos FROM evento WHERE TM_idTM = $idTM AND HoraInicio = '$dateTime' ";
+		// var_dump($query);
+		$res = mysql_query ( $query ) or die ( mysql_error () ); // ejecutar la query
+		if (mysql_affected_rows () >= 1) { // si hay algun error
+			$result = false;
+		} else {
+			$result = true;
+		}
+	} else {
+		$query = "SELECT Ecos.Nombre, Centro.Nombre FROM evento, ecos, centro WHERE TM_idTM = $idTM AND HoraInicio = '$dateTime' AND Ecos_idEcos = idEcos AND Centro_idCentro = idCentro";
+		// var_dump($query);
+		$res = mysql_query ( $query ) or die ( mysql_error () ); // ejecutar la query
+		if (mysql_affected_rows () >= 1) { // si hay algun error
+			while ( $row = mysql_fetch_assoc ( $res ) ) {
+				$result [] = $row;
+			} // se guardan los valores en $result
+		} else {
+			$result = true;
+		}
+	}
+	
+	if ($type = 'json') {
+		return json_encode ( $result );
+	} else {
+		return $result;
+	}
+}
 
+//echo verifyEco(1, '2015-07-27 09:00:00', 'json', true);//coincidencia de evento sin display
+// echo verifyTM(2, '2015-07-27 09:00:00', 'array',true);
 ?>
