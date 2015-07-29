8872373be2cd2031c616c0473e5b9f31e187c02b
Cesar Gonzalez <cgonzb@gmail.com>
2015-07-29 17:59:06 -0300

    verifica el TM y se entrega el id del TM al evento

--- querys/getEventos.php
+++ querys/getEventos.php
@@ -7,7 +7,7 @@
 include_once dirname ( __FILE__ ) . '/../conexionLocal.php'; // archivo de conexion local
 function getEventos($idCentro = null) {
 	if ($idCentro != null) {
-		$query = "SELECT Ecos.Nombre as title, concat(TM.Nombre,' ' ,TM.Apellido) as description, idEvento as id, HoraInicio as start, HoraTermino as end, color, idEcos as idEco
+		$query = "SELECT Ecos.Nombre as title, concat(TM.Nombre,' ' ,TM.Apellido) as description, idEvento as id, HoraInicio as start, HoraTermino as end, color, idEcos as idEco, idTM
 				FROM Evento, Ecos, TM
 				WHERE TM_idTM=idTM AND Ecos_idEcos=idEcos AND Centro_idCentro=$idCentro";
 		
