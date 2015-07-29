4a365715f324644a27142215864de7e625214a1b
Cesar Gonzalez <cgonzb@gmail.com>
2015-07-29 19:38:42 -0300

    se hace un get de los eventos de un TM

---
+++ querys/getEventosTM.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * getEcosTM funcion que se conecta al a base de datos para entregar los Eventos de un TM
+ * se le debe entregar un idTM
+ *
+ */
+include_once dirname ( __FILE__ ) . '/../conexionLocal.php'; // archivo de conexion local
+function getEventosTM($idTM = null) {
+	if ($idTM != null) {
+		$query = "SELECT idEvento as id, HoraInicio as start, HoraTermino as end, color, idEcos as idEco, Ecos.Nombre as title, Centro.Nombre as description
+				FROM Evento, Ecos, TM, Centro
+				WHERE TM_idTM=$idTM AND TM_idTM=idTM AND Ecos_idEcos=idEcos AND Centro_idCentro=idCentro";
+		
+		$res = mysql_query ( $query ) or die ( mysql_error () );
+				
+		while ( $row = mysql_fetch_assoc ( $res ) ) {
+			$result [] = $row;
+		} // while
+		return $result;
+	} // si se le entrega correctamente el idCentro
+}
+//var_dump ( getEventosTM ( 1 ) );
+?>
