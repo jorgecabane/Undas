e806ca5d62b30dc36ffa9e785a62c71f69204961
Cesar Gonzalez <cgonzb@gmail.com>
2015-08-03 13:24:05 -0300

    se elimino algunos divs
    para corregir el display de la informacion dentro del marco que
    corresponde. (BUG)

--- perfil/informacionTm.php
+++ perfil/informacionTm.php
@@ -1,112 +1,99 @@
-<center>
-	<section class="productList">
-		</head>
-		<body>
-			<div align="center">
-            <?php
-												include_once "../include/isAdmin.php";
-												session_start ();
-												if ($_SESSION ["usuario"]) {
-													if (isAdmin ( $_SESSION ["idusuario"] ) == 1) {
-														$admin = 1;
-													} else {
-														$admin = 0;
-													}
+<div align="center">
+           <?php
+											include_once "../include/isAdmin.php";
+											session_start ();
+											if ($_SESSION ["usuario"]) {
+												if (isAdmin ( $_SESSION ["idusuario"] ) == 1) {
+													$admin = 1;
+												} else {
+													$admin = 0;
 												}
+											}
+											
+											$resultado = mysql_query ( "SELECT * from TM Where Rut=$rut" ) or die ( mysql_error () );
+											
+											if ($resultado) {
 												
-												$resultado = mysql_query ( "SELECT * from TM Where Rut=$rut" ) or die ( mysql_error () );
-												
-												if ($resultado) {
-													
-													echo "<table id='t01' class='table table-hover table-bordered'>";
-													echo "<thead><tr>";
-													echo "<th>Nombre</th>";
-													echo "<th>Apellido</th>";
-													echo "<th>Rut</th>";
-													echo "<th>Mail</th>";
-													echo "<th>Celular</th>";
-													if ($admin == 1) {
-														echo "<th>Editar</th>";
-														echo "<th>Eliminar</th>";
-													}
-													echo "</thead><tbody>";
-													while ( $row = mysql_fetch_array ( $resultado ) ) {
-														?>
+												echo "<table id='t01' class='table table-hover table-bordered'>";
+												echo "<thead><tr>";
+												echo "<th>Nombre</th>";
+												echo "<th>Apellido</th>";
+												echo "<th>Rut</th>";
+												echo "<th>Mail</th>";
+												echo "<th>Celular</th>";
+												if ($admin == 1) {
+													echo "<th>Editar</th>";
+													echo "<th>Eliminar</th>";
+												}
+												echo "</thead><tbody>";
+												while ( $row = mysql_fetch_array ( $resultado ) ) {
+													?>
 														
 															
 					<tr>
-					<td>
-						<div class="form-group">
-							<input id="nombre" type="text" class="form-control editable"
-								name="Nombre" value="<?php echo $row['Nombre'];?>"
-								<?php if($admin==0){ echo "disabled='disabled'";       }?>
-								required>
-						</div>
-					</td>
-					<td>
-						<div class="form-group">
-							<input id="apellido" type="text" class="form-control editable"
-								name="Apellido" value="<?php echo $row['Apellido'];?>"
-								<?php if($admin==0){ echo "disabled='disabled'";       }?>
-								required>
-						</div>
-					</td>
-					<td>
-						<div class="form-group">
-							<input id="rut" type="number" class="form-control editable"
-								name="Rut" value="<?php echo $row['Rut'];?>"
-								<?php if($admin==0){ echo "disabled='disabled'";       }?>
-								required>
-						</div>
-					</td>
-					<td>
-						<div class="form-group">
-							<input id="mail" type="text" class="form-control editable"
-								name="Mail" value="<?php echo $row['Mail'];?>"
-								<?php if($admin==0){ echo "disabled='disabled'";       }?>
-								required>
-						</div>
-					</td>
-					<td>
-						<div class="form-group">
-							<input id="celular" type="number" class="form-control editable"
-								name="Celular" value="<?php echo $row['Celular'];?>"
-								<?php if($admin==0){ echo "disabled='disabled'";       }?>
-								required>
-						</div>
-					</td>
+		<td>
+			<div class="form-group">
+				<input id="nombre" type="text" class="form-control editable"
+					name="Nombre" value="<?php echo $row['Nombre'];?>"
+					<?php if($admin==0){ echo "disabled='disabled'";       }?> required>
+			</div>
+		</td>
+		<td>
+			<div class="form-group">
+				<input id="apellido" type="text" class="form-control editable"
+					name="Apellido" value="<?php echo $row['Apellido'];?>"
+					<?php if($admin==0){ echo "disabled='disabled'";       }?> required>
+			</div>
+		</td>
+		<td>
+			<div class="form-group">
+				<input id="rut" type="number" class="form-control editable"
+					name="Rut" value="<?php echo $row['Rut'];?>"
+					<?php if($admin==0){ echo "disabled='disabled'";       }?> required>
+			</div>
+		</td>
+		<td>
+			<div class="form-group">
+				<input id="mail" type="text" class="form-control editable"
+					name="Mail" value="<?php echo $row['Mail'];?>"
+					<?php if($admin==0){ echo "disabled='disabled'";       }?> required>
+			</div>
+		</td>
+		<td>
+			<div class="form-group">
+				<input id="celular" type="number" class="form-control editable"
+					name="Celular" value="<?php echo $row['Celular'];?>"
+					<?php if($admin==0){ echo "disabled='disabled'";       }?> required>
+			</div>
+		</td>
 						<?php
-														if ($admin == 1) {
-															?>
+													if ($admin == 1) {
+														?>
 															
 						<td>
-						<div>
-							<input type="hidden" name="id"
-								value="<?php echo $row['idTM']; ?>" /> <input type="submit"
-								value="Finalizar edicion" class='btn btn-info btnedit'
-								disabled="disabled" />
-						</div>
-					</td>
+			<div>
+				<input type="hidden" name="id" value="<?php echo $row['idTM']; ?>" />
+				<input type="submit" value="Finalizar edicion"
+					class='btn btn-info btnedit' disabled="disabled" />
+			</div>
+		</td>
 
 
-					</td>
-					<td><input type="submit" value="Eliminar TM"
-						class='btn btn-danger btnerase'></td>
-				</tr> <?php }?>
-					<?php
-													}
-													?>
-
-					</tbody>
-				</table>
+		</td>
+		<td><input type="submit" value="Eliminar TM"
+			class='btn btn-danger btnerase'></td>
+	</tr> <?php }?>
 					<?php
 												}
 												?>
+
+					</tbody>
+	</table>
+					<?php
+											}
+											?>
 			
 			</div>
-		</body>
-	</section>
-</center>
 
 <script>
 $(".editable").keyup(function(){
--- perfil/perfilGeneral.php
+++ perfil/perfilGeneral.php
@@ -1,48 +1,31 @@
 <?php
-include "../conexionLocal.php";
-
+include_once "../conexionLocal.php";
 $rut = $_POST ['Rut'];
 
 ?>
+<div class="row">
+	<ul class="nav nav-tabs">
+		<li class="nav active"><a href="#A" data-toggle="tab">Info</a></li>
+		<li class="nav"><a href="#B" data-toggle="tab">Cobros</a></li>
+		<li class="nav"><a href="#C" data-toggle="tab">Horario</a></li>
+	</ul>
 
 
-<div align="center">
-	
-</div>
-</div>
-<br>
-<center>
-	<div class="container">
-		<ul class="nav nav-tabs">
-			<li class="nav active"><a href="#A" data-toggle="tab">Info</a></li>
-			<li class="nav"><a href="#B" data-toggle="tab">Cobros</a></li>
-			<li class="nav"><a href="#C" data-toggle="tab">Horario</a></li>
-		</ul>
+	<!-- Tab panes -->
+	<div class="tab-content">
+		<div class="tab-pane fade in active" id="A">
+			<center><h3>Informacion Trabajador</h3></center>
+			 <?php require_once "informacionTm.php";?>
+        </div>
 
+		<div class="tab-pane fade" id="B">
+			<center><h3>Cobros por Centro</h3></center>
+			<?php include_once "informacionCobro.php";?>
+		</div>
 
-		<!-- Tab panes -->
-		<div class="tab-content">
-			<div class="tab-pane fade in active" id="A">
-			<h3>Informacion Trabajador</h3>
-			 <?php
-			include "informacionTm.php";
-			?>
-                    </table>
-			</div>
-
-			<div class="tab-pane fade" id="B">
-				<h3>Cobros por Centro</h3>
-				<br>
-
-<?php
-
-include "informacionCobro.php";
-?>
-			</div>
-
-			<div class="tab-pane fade" id="C"><h3>Horario TM</h3></div>
+		<div class="tab-pane fade" id="C">
+			<center><h3>Horario TM</h3></center>
+			<?php require_once "informacionHorario.php"//informacion del calendario personal del TM?>
 		</div>
 	</div>
-
-
-
+</div>
