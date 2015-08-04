f6ee9a740ea6b40e596d26858514258dad91aacb
jorgecabane93 <jorgecabane93@gmail.com>
2015-08-04 15:43:15 -0400

    agregar tm ajax

--- perfil/informacionCobro.php
+++ perfil/informacionCobro.php
@@ -81,3 +81,4 @@
 		</body>
 	</section>
 </center>
+
--- querys/insertTmR.php
+++ querys/insertTmR.php
@@ -7,32 +7,13 @@ $rut = $_POST ['rut'];
 $mail = $_POST ['mail'];
 $celular = $_POST ['celular'];
 $contraseña = $_POST ['contrasena'];
-$repetircontraseña = $_POST ['repetircontrasena'];
 
-if ($contraseña == $repetircontraseña) {
+
+
 	// comprobamos si ha ocurrido un error.
 	$query = "insert into TM values (null,'$nombre','$apellido','$rut','$mail','$celular','$contraseña',0)";
 	$resultado2 = mysql_query ( $query );
 	if ($resultado2) {
 		echo "Perfecto, redireccionando";
-		?>
-
-<meta http-equiv="Refresh" content="1;url=../agregarTmR.php">
-<?php
-	} else {
-		echo "Error el rut ya existe, intente denuevo";
-		?>
-
-<meta http-equiv="Refresh" content="1;url=../agregarTmR.php">
-<?php
 	}
-} else {
-	echo " Las contraseñas no coinciden, redireccionando";
-	?>
-<meta http-equiv="Refresh" content="1;url=../agregarTmR.php">
-; 
-    <?php
-}
-
-?>
-    
+	
