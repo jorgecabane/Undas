fb7c33ae459380cc73685ed958398242a0e9b414
Cesar Gonzalez <cgonzb@gmail.com>
2015-08-05 23:20:03 -0300

    corregido bug al editar cobro
    el ajax estaba dentro del .keyup() por lo que se ejecutaba cada vez que
    se tecleaba.

--- perfil/informacionCobro.php
+++ perfil/informacionCobro.php
@@ -4,35 +4,35 @@
 		<body>
 			<div align="center">
             <?php
-			include_once "../include/isAdmin.php";
-			
-			if ($_SESSION ["usuario"]) {
-				if (isAdmin ( $_SESSION ["idusuario"] ) == 1) {
-					$admin = 1;
-				} else {
-					$admin = 0;
-				}
-			}
-			
-			$resultado = mysql_query ( "SELECT ValorHora.Valor as Valor, ValorHora.Semana as Semana, Centro.Nombre as Centro, TM.idTM as idTM from TM inner join ValorHora on TM.idTM = ValorHora.Tm_idTM inner join Centro on Centro.idCentro = ValorHora.Centro_idCentro Where TM.Rut=$rut" ) or die ( mysql_error () );
-			
-			if ($resultado) {
-				
-				echo "<table id='t01'class='table table-hover table-bordered table-condensed'>";
-				echo "<thead><tr>";
-				echo "<th>Centro</th>";
-				echo "<th>Cobro</th>";
-				echo "<th>Semana/Sabado</th>";
-				if ($admin == 1) {
-					echo "<th>Editar</th>";
-					echo "<th>Eliminar</th></tr>";
-				}
-				echo "</thead><tbody>";
-				while ( $row = mysql_fetch_array ( $resultado ) ) {
-					
-					echo "<tr>";
-					echo "<td class='centro'>" . $row ['Centro'] . "</td>\n";
-					?>
+												include_once "../include/isAdmin.php";
+												
+												if ($_SESSION ["usuario"]) {
+													if (isAdmin ( $_SESSION ["idusuario"] ) == 1) {
+														$admin = 1;
+													} else {
+														$admin = 0;
+													}
+												}
+												
+												$resultado = mysql_query ( "SELECT ValorHora.Valor as Valor, ValorHora.Semana as Semana, Centro.Nombre as Centro, TM.idTM as idTM from TM inner join ValorHora on TM.idTM = ValorHora.Tm_idTM inner join Centro on Centro.idCentro = ValorHora.Centro_idCentro Where TM.Rut=$rut" ) or die ( mysql_error () );
+												
+												if ($resultado) {
+													
+													echo "<table id='t01'class='table table-hover table-bordered table-condensed'>";
+													echo "<thead><tr>";
+													echo "<th>Centro</th>";
+													echo "<th>Cobro</th>";
+													echo "<th>Semana/Sabado</th>";
+													if ($admin == 1) {
+														echo "<th>Editar</th>";
+														echo "<th>Eliminar</th></tr>";
+													}
+													echo "</thead><tbody>";
+													while ( $row = mysql_fetch_array ( $resultado ) ) {
+														
+														echo "<tr>";
+														echo "<td class='centro'>" . $row ['Centro'] . "</td>\n";
+														?>
 														<td>
 					<div class="form-group">
 						<input class='editableCobro' type="text" class="form-control"
@@ -44,14 +44,14 @@
 				</td>
 							<?php
 														
-							if ($row ['Semana'] == 1) {
-								echo "<td class='semana'>Semana</td>";
-							} else {
-								
-								echo "<td class='semana'>Sabado</td>";
-							}
-							if ($admin == 1) {
-							?>
+														if ($row ['Semana'] == 1) {
+															echo "<td class='semana'>Semana</td>";
+														} else {
+															
+															echo "<td class='semana'>Sabado</td>";
+														}
+														if ($admin == 1) {
+															?>
 <td><input type="hidden" id="idTM" name="id"
 					value="<?php echo $row['idTM']; ?>" /> <input type="submit"
 					value="Editar" class='btn btn-info btneditable' disabled="disabled" />
@@ -77,75 +77,40 @@
 <script>
 $(".editableCobro").keyup(function(){
 //$(".btneditable").removeAttr("disabled");
+//solo se buscan los elementos de la fila seleccionada
+var row = $(this).parent().parent().parent();
 
-$(this)
-.parent()
-.parent()
-.parent()
-.children()
-.children(".btneditable")
-.removeAttr("disabled");
-
-
- $(this)
- .parent()
- .parent()
- .parent()
- .addClass("danger");
-
-
-var input = 
-	 $(this)
-	 .parent()
-	 .parent()
-	 .parent()
-	 .children()
-	 .children()
-	 .children(".editableCobro");
-
-var  centro=
-	 $(this)
-	 .parent()
-	 .parent()
-	 .parent()
-	 .children(".centro");
-
-var  semana=
-	 $(this)
-	 .parent()
-	 .parent()
-	 .parent()
-	 .children(".semana");
+row.find(".btneditable").removeAttr("disabled");
+row.addClass("danger");
+});
 
 $(".btneditable").click(function(){
+	//solo se buscan los elementos de la fila seleccionada
+	var row = $(this).parent().parent().parent();
+	var input = row.find(".editableCobro");
+	var centro= row.find(".centro");
+	var semana= row.find(".semana");
 
-	 jQuery.ajax({
-	       method: "POST",
-	       url: "querys/updateCobro.php",
-	       data: {
-		     		'valor': input.val(),
-		     		'id': $("#idTM").val(),
-		     		'semana': semana.html(),
-		     		'centro':  centro.html()
+		jQuery.ajax({
+		       method: "POST",
+		       url: "querys/updateCobro.php",
+		       data: {
+			     		'valor': input.val(),
+			     		'id': $("#idTM").val(),
+			     		'semana': semana.html(),
+			     		'centro':  centro.html()
 
-	       },
-	       
-	       success: function(response)
-	       {
-	    	   $(".btneditable").attr("disabled","disabled");
-	           semana.parent()
-		       .removeClass("danger")
-		       .addClass("success");
-	       }
-
-	 });
-
-
-});
-
-
- 
-});
+		       },
+		       
+		       success: function(response)
+		       {
+		    	   $(".btneditable").attr("disabled","disabled");
+		           semana.parent()
+			       .removeClass("danger")
+			       .addClass("success");
+		       }//success
+		});//ajax
+});//click .btneditable
 </script>
 
 
