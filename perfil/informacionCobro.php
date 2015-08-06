d58ed2e1eec547d49c66a0707c4e76a134144fa3
Cesar Gonzalez <cgonzb@gmail.com>
2015-08-05 22:39:20 -0300

    poquito de organizacion mientras se corrije el bug

--- perfil/informacionCobro.php
+++ perfil/informacionCobro.php
@@ -4,68 +4,61 @@
 		<body>
 			<div align="center">
             <?php
-												include_once "../include/isAdmin.php";
-												
-												if ($_SESSION ["usuario"]) {
-													if (isAdmin ( $_SESSION ["idusuario"] ) == 1) {
-														$admin = 1;
-													} else {
-														$admin = 0;
-													}
-												}
-												
-												$resultado = mysql_query ( "SELECT ValorHora.Valor as Valor, ValorHora.Semana as Semana, Centro.Nombre as Centro, TM.idTM as idTM from TM inner join ValorHora on TM.idTM = ValorHora.Tm_idTM inner join Centro on Centro.idCentro = ValorHora.Centro_idCentro Where TM.Rut=$rut" ) or die ( mysql_error () );
-												
-												if ($resultado) {
-													
-													echo "<table id='t01'class='table table-hover table-bordered table-condensed'>";
-													echo "<thead><tr>";
-													echo "<th>Centro</th>";
-													echo "<th>Cobro</th>";
-													echo "<th>Semana/Sabado</th>";
-													if ($admin == 1) {
-														echo "<th>Editar</th>";
-														echo "<th>Eliminar</th></tr>";
-													}
-													echo "</thead><tbody>";
-													while ( $row = mysql_fetch_array ( $resultado ) ) {
-														
-														echo "<tr>";
-														echo "<td class='centro'>" . $row ['Centro'] . "</td>";
-														?>
+			include_once "../include/isAdmin.php";
+			
+			if ($_SESSION ["usuario"]) {
+				if (isAdmin ( $_SESSION ["idusuario"] ) == 1) {
+					$admin = 1;
+				} else {
+					$admin = 0;
+				}
+			}
+			
+			$resultado = mysql_query ( "SELECT ValorHora.Valor as Valor, ValorHora.Semana as Semana, Centro.Nombre as Centro, TM.idTM as idTM from TM inner join ValorHora on TM.idTM = ValorHora.Tm_idTM inner join Centro on Centro.idCentro = ValorHora.Centro_idCentro Where TM.Rut=$rut" ) or die ( mysql_error () );
+			
+			if ($resultado) {
+				
+				echo "<table id='t01'class='table table-hover table-bordered table-condensed'>";
+				echo "<thead><tr>";
+				echo "<th>Centro</th>";
+				echo "<th>Cobro</th>";
+				echo "<th>Semana/Sabado</th>";
+				if ($admin == 1) {
+					echo "<th>Editar</th>";
+					echo "<th>Eliminar</th></tr>";
+				}
+				echo "</thead><tbody>";
+				while ( $row = mysql_fetch_array ( $resultado ) ) {
+					
+					echo "<tr>";
+					echo "<td class='centro'>" . $row ['Centro'] . "</td>\n";
+					?>
 														<td>
 					<div class="form-group">
-						<input class='editableCobro' type="text" class="form-control" name="Mail"
-							value="<?php echo $row['Valor'];?>"
+						<input class='editableCobro' type="text" class="form-control"
+							name="Mail" value="<?php echo $row['Valor'];?>"
 							<?php if($admin==0){ echo "disabled='disabled'";       }?>
 							required>
 
 					</div>
 				</td>
-							 <?php
+							<?php
 														
-														
-														
-														
-														if ($row ['Semana'] == 1) {
-															echo "<td class='semana'>Semana</td>";
-														} else {
-															
-															echo "<td class='semana'>Sabado</td>";
-														}
-														if ($admin == 1) {
-															?>
-<td>
-					
-						<input type="hidden" id="idTM" name="id" value="<?php echo $row['idTM']; ?>" />
-						<input  type="submit" value="Editar" class='btn btn-info btneditable' disabled="disabled" />
-					
+							if ($row ['Semana'] == 1) {
+								echo "<td class='semana'>Semana</td>";
+							} else {
+								
+								echo "<td class='semana'>Sabado</td>";
+							}
+							if ($admin == 1) {
+							?>
+<td><input type="hidden" id="idTM" name="id"
+					value="<?php echo $row['idTM']; ?>" /> <input type="submit"
+					value="Editar" class='btn btn-info btneditable' disabled="disabled" />
+
 				</td>
-				<td>
-					
-						<input type="submit" value="Eliminar" class='btn btn-danger btndelete' />
-					
-				</td>
+				<td><input type="submit" value="Eliminar"
+					class='btn btn-danger btndelete' /></td>
 <?php
 														}
 														
@@ -165,16 +158,14 @@ $(".btndelete").click(function(){
 		 $(this)
 		 .parent()
 		 .parent()
-		 .children(".centro")
+		 .find(".centro")
 		 .html();
 
 	var input = 
 		 $(this)
 	     .parent()
 	     .parent()
-	     .children()
-	     .children()
-	     .children(".editableCobro")
+	     .find(".editableCobro")
 	     .val();
 
 	var  semana=
@@ -186,8 +177,7 @@ $(".btndelete").click(function(){
 	 
 	var r = confirm("Esta seguro que quiere eliminar la fila: "+ centro +" valor: "+ input +"?");
 	if (r == true) {
-
-		 jQuery.ajax({
+		jQuery.ajax({
 		       method: "POST",
 		       url: "querys/eraseCobro.php",
 		       data: {			     		
