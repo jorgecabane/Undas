85c69ee1f166977f30c33a9532fd6d9150ccea98
jorgecabane93 <jorgecabane93@gmail.com>
2015-08-05 21:35:32 -0300

    ayuuuuuuuuuuuda

--- agregarCentroNuevoR.php
+++ agregarCentroNuevoR.php
@@ -18,22 +18,13 @@ and open the template in the editor.
 <body background="images/bg.gif">
 	<div class="container">
 		<h2>Agregar Centro Nuevo</h2>
-		<form action='querys/insertCentroNuevoR.php' method='POST'>
+	<?php //	<form action='querys/insertCentroNuevoR.php' method='POST'> ?>
 			<div class="form-group">
-				<label for="nombre">Nombre</label> <input type="text"
-					class="form-control" name="nombre" placeholder="Agrege nombre"
-					required>
-			</div>
-			<div class="form-group">
-				<label for="siglas">Siglas</label> <input type="text"
-					class="form-control" name="siglas" placeholder="Agrege siglas"
-					required>
-			</div>
-			<div class="form-group">
-				<label for="siglas">Numero de ecos</label> <input type="number"
-					class="form-control" name="ecos"
-					placeholder="Agrege numero de ecos" required>
-			</div>
+			<label for="nombre">Nombre</label> <input type="text"
+				class="form-control" id="nombre" placeholder="Agrege nombre"
+				required>
+		</div>
+		<div class="form-group">
 			<label>Empresa a la que pertenece</label> <br>
          
           <?php
@@ -50,15 +41,45 @@ and open the template in the editor.
 												echo "<option value='" . $row ['idEmpresa'] . "'>" . $row ['Nombre'] . "</option>";
 											}
 											echo "</select><br>";
-											echo "<br>";
-											echo "<input class='btn btn-success' type='submit' value='Agregar'>";
 											
-											echo "</form>";
+											// echo "</form>";
 										}
 										?>
     
 
+        </div>
+		<div class="form-group">
+			<label for="siglas">Siglas</label> <input type="text"
+				class="form-control" id="siglas" placeholder="Agrege siglas"
+				required>
+		</div>
+		<div class="form-group">
+			<label for="Ecos">Numero de ecos</label> <input type="number"
+				class="form-control" id="ecos" placeholder="Agrege numero de ecos"
+				required>
+		</div>
 
+		<div class="row">
+			<div id="append" class="col-xs-2">
+			
+			</div>
+		</div>
+		<div class="form-group">
+		    <br>
+			<input class='btn btn-info btnedit ' type='submit' value='Agregar'>
+		</div>
 
 </body>
+
 </html>
+<script>
+
+	$( "#ecos" ).keyup(function(event){
+		event.preventDefault();
+	for( var i = 1 ; i<= $( "#ecos" ).val() ; i++){
+		$( "#append" ).append( "<input type='Text' class='form-control' class='ecos' Value='Eco'"+ i +"required>" );
+			
+	}
+	  
+   });
+ </script>
