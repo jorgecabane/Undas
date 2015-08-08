e11a339a545a86b3f1117847298cd8782f193782
sikeze <mipozarski@alumnos.uai.cl>
2015-08-08 17:26:09 -0300

    Recuperacion clave
    Se genera la recuperacion de la clave con la generacion de una nueva

--- enviarPassword.php
+++ enviarPassword.php
@@ -4,51 +4,41 @@ include_once "conexionLocal.php";
 
 $email=$_POST['email'];
 $rut=$_POST['rut'];
-	
-	$query="SELECT * FROM TM WHERE Rut=$rut AND Mail like '$email'";
-	
-	$result   = mysql_query($query);
-	
-	
-	if($result){
-	
-	$rows=mysql_fetch_array($result);
-	
-	$pass  =  $rows['Password'];//FETCHING PASS
-	//echo "your pass is ::".($pass)."";
-	$to = $rows['Mail'];
-	//echo "your email is ::".$email;
-	//Details for sending E-mail
-	$from = "TmTecnomed";
-			$url = "http://tmtecnomed.cl//";
-			$body  =  "TmTecnomed Recuperacion de clave
-			Url : $url;
-			email Details is : $to;
-			Here is your password  : $pass;
-			lo saluda,
-			TmTecnomed";
-			$from = "soportetecnico@tmtecnomed.cl";
-			$subject = "Tmtecnomed clave recuperada";
-			$headers1 = "From: $from\n";
-			$headers1 .= "Content-type: text/html;charset=iso-8859-1\r\n";
-			$headers1 .= "X-Priority: 1\r\n";
-			$headers1 .= "X-MSMail-Priority: High\r\n";
-			$headers1 .= "X-Mailer: Just My Server\r\n";
-			$sentmail = mail ( $to, $subject, $body, $headers1 );
-	} else {
-			if ($_POST ['email'] != "") {
-				echo "<span style='color: #ff0000;'> Not found your email in our database</span>";
-			}
+
+$query="SELECT * FROM TM Where Rut=$rut";
+		$result=mysql_query($query);
+		if($result){
+			$row=mysql_fetch_assoc($result);
+			
+			 if($row['Mail']==$email){
+			 	$random=rand(1000000,9999999);
+			 	$apellido=$row['Apellido'];
+			 	$nuevapassword=$apellido.$random;
+			 	echo $nuevapassword;
+			 	$query2="UPDATE TM SET Password='$nuevapassword' WHERE Rut=$rut";
+			 			$resultado2=mysql_query($query2);
+			 	if($resultado2){
+			 
+			 	
+		 		$to = $row['Mail'];
+		 		$subject = "Recuperacion de contraseña";
+		 		$txt = "tu nueva contraseña es:$nuevapassword";
+		 		$headers = "From: serviciotenico@tmtecnomed.cl" . "\r\n";
+		 	
+		 		mail($to,$subject,$txt,$headers);
+			 	}
+			 	else{
+			 		echo "no se updatea la password";
+			 	}
+			echo "Revise el correo que le otorgo tmtecnomed";
+		 	}
+		 	else
+		 	{
+		 	echo "correo incorrecto";
+		 	}
 		}
-		//If the message is sent successfully, display sucess message otherwise display an error message.
-		if($sentmail==1)
-			{
-				echo "<span style='color: #ff0000;'> Your Password Has Been Sent To Your Email Address.</span>";
-			}
-		else
-			{
-				if($_POST['email']!="")
-					echo "<span style='color: #ff0000;'> Cannot send password to your e-mail address.Problem with sending mail...</span>";
-			}
+else {
+	echo "El mail no esta registrado en nuestra base de datos";
+}
 
 ?>
