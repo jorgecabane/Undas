656f3361786f5849a5862a48f0e369a4ebeea1e0
jorgecabane93 <jorgecabane93@gmail.com>
2015-08-11 22:00:11 -0300

    sin tab info

--- empresaGeneral.php
+++ empresaGeneral.php
@@ -1,30 +1,17 @@
 <?php
-session_start();
+session_start ();
 include "header.php";
 
 ?>
-<div align="center">
-	
-</div>
-</div>
-<br>
-<center>
+
+
+
+
 	<div class="container">
-		<ul class="nav nav-tabs">
-			<li class="nav active"><a href="#A" data-toggle="tab">Info</a></li>
-		</ul>
 
-
-		<!-- Tab panes -->
-		<div class="tab-content">
-			<div class="tab-pane fade in active" id="A">
-			<h3>Informacion Trabajador</h3>
+		<h2>Informacion Trabajador</h2>
 			 <?php
-			include "perfil/informacionEmpresas.php";
-			?>
+				include "perfil/informacionEmpresas.php";
+				?>
                     </table>
-			</div>
-
-		
-
-
+	</div>
