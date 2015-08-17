6a5ac9834ef00cb7c9bcb799b944538c2b7df69e
jorgecabane93 <jorgecabane93@gmail.com>
2015-08-17 02:30:20 -0300

    funcionaa

--- querys/insertTmR.php
+++ querys/insertTmR.php
@@ -5,13 +5,15 @@ $nombre = $_POST ['nombre'];
 $apellido = $_POST ['apellido'];
 $rut = $_POST ['rut'];
 $mail = $_POST ['mail'];
+$cuenta = $_POST ['cuenta'];
+$banco = $_POST ['banco'];
 $celular = $_POST ['celular'];
 $contrasena = $_POST ['contrasena'];
 
 
 
 	// comprobamos si ha ocurrido un error.
-	$query = "insert into TM values (null,'$nombre','$apellido','$rut','$mail','$celular','$contrasena',0)";
+	$query = "insert into TM values (null,'$nombre','$apellido','$rut','$mail',$celular,'$contrasena',0, '$cuenta','$banco')";
 	$resultado2 = mysql_query ( $query );
 	if ($resultado2) {
 		echo "Perfecto, redireccionando";
