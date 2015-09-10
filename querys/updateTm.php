0a72a7b8c8d2208b0e5bddf9659a62738f50abe7
Cesar Gonzalez <cgonzb@gmail.com>
2015-09-09 22:04:32 -0300

    correccion

--- querys/updateTm.php
+++ querys/updateTm.php
@@ -1,24 +1,23 @@
 <?php
+
 include_once "../conexionLocal.php";
 
-$nombre=$_POST['nombre'];
-$apellido=$_POST['apellido'];
-$rut=$_POST['rut'];
-$mail=$_POST['mail'];
-$celular=$_POST['celular'];
-$banco=$_POST['banco'];
-$cuenta=$_POST['cuenta'];
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$rut = $_POST['rut'];
+$mail = $_POST['mail'];
+$celular = $_POST['celular'];
+$banco = $_POST['banco'];
+$cuenta = $_POST['cuenta'];
 
-$query="UPDATE tm SET Nombre='$nombre', Apellido='$apellido', Rut='$rut', Mail='$mail', Celular=$celular, Banco='$banco', Cuentacorriente=$cuenta WHERE Rut='$rut'";
+$query = "UPDATE tm SET Nombre='$nombre', Apellido='$apellido', Rut='$rut', Mail='$mail', Celular=$celular, Banco='$banco', Cuentacorriente=$cuenta WHERE Rut='$rut'";
 
-$resultado=mysql_query($query);
-if($resultado) {
-	//success
-	echo"Actualizado con exito, redireccionando";
-
+$resultado = mysql_query($query);
+if ($resultado) {
+    //success
+    echo"Actualizado con exito, redireccionando";
 } else {
     //failure
     echo " Se produjo un error en la actualizacion, redireccionando";
-
 }
 
