239c04206d042fec3a1dbdc1c119c594e1812a88
jorgecabane93 <jorgecabane93@gmail.com>
2015-09-09 23:26:45 -0300

    mayore cantidad de orden :(

--- Include/disponibles.php
+++ Include/disponibles.php
@@ -4,20 +4,11 @@
  * se muestra el listado de personas disponibles con la funcion getDisponibles($start,$end);
  */
 require_once dirname ( __FILE__ ) . "/../querys/getDisponibles.php";
-require_once dirname ( __FILE__ ) . "/../querys/getDisponiblesHora.php";
-if(isset($_POST['start'])){
+
 $start = $_POST['start'];
 $end = $_POST['end'];
 
 $disponibles = getDisponibles($start, $end);
 echo json_encode($disponibles);
-}
-if(isset($_POST['dia'])){
-	$dia = $_POST['dia'];
-	$horaend = $_POST['horaend'];
-	$horastart = $_POST['horastart'];
-	
-	$disponibles = getDisponiblesHora($dia, $horaend,$horastart);
-	echo json_encode($disponibles);
-}
+
 ?>
