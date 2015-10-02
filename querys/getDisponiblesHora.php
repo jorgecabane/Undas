1888ad3a156c7798ed9932ca56dc56038529a389
jorgecabane93 <jorgecabane93@gmail.com>
2015-10-02 17:02:42 -0300

    arreglo widget

--- querys/getDisponiblesHora.php
+++ querys/getDisponiblesHora.php
@@ -19,7 +19,7 @@ function getDisponiblesHora($dia, $horastart, $horaend) {
 	FROM tm
 	WHERE idTM NOT IN (SELECT DISTINCT(TM_idTM)
 	FROM tm, evento
-	WHERE tm.idTM = TM_idTM AND (DATE(HoraInicio) BETWEEN '$dia' AND '$dia')) AND (TIME(HoraInicio) BETWEEN '$horastart' AND '$horaend')";
+	WHERE tm.idTM = TM_idTM AND (DATE(HoraInicio) BETWEEN '$dia' AND '$dia')) AND (TIME(HoraInicio) BETWEEN '$horastart' AND '$horaend') 	And Doctor= 0 And Centro =0";
 	$res = mysql_query($query) OR DIE(mysql_error());
 	if ($res) {
 		$res = mysql_query($query) or die(mysql_error());
