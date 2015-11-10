e995d11f1a4268b4098d8f9a57b55eac4c0faf24
Cesar Gonzalez <cgonzb@gmail.com>
2015-11-10 20:10:43 -0300

    se agrega well a calendario Centro

--- centros/informacionHorario.php
+++ centros/informacionHorario.php
@@ -7,7 +7,7 @@ $centro = $_GET ['centro'];
 <!-- archivo de carga del calendario personal del TM sin opcion de modificar (solo exportacion) -->
 <div class='container-fluid'>
     <div class='row'>
-     <div class='col-md-12 well well-sm well-titles'>
+        <div class='col-md-12 well well-sm well-titles'>
             <div class="row">
                 <center>
                     <h2>
@@ -19,41 +19,45 @@ $centro = $_GET ['centro'];
 
             </div>
         </div>
-        </div>
-        
-
-              </div>
-                       <div class="col-sm-1 hidden-print">
+    </div>
+    <div class="row">
+        <div class="col-sm-12 well well-sm">
+            <div class="col-sm-1 hidden-print">
                 <button class="btn btn-danger btn-block" onClick="window.print()" id="descargar" data-toggle="tooltip" data-placement="left" title="Descargar PDF!">
                     <span class="glyphicon glyphicon-print"></span>
                 </button>
             </div>
-<div id='calendar' ></div>
+            <div id='calendar' ></div>
+        </div>
+    </div>
+</div>
+
 
 
 <script>
-                        $(document).ready(function() {
-                            $('#calendar').fullCalendar({
-                                eventSources: {
-                                    url: "Include/feedEventosCentro.php?idCentro=<?php echo $idCentro; ?>"
-                                },
-                                header: {
-                                    left: 'prev,today,next',
-                                    center: 'title',
-                                    right: 'agendaDay,agendaWeek,month'
-                                },
-                                eventRender: function(event, element) {
-                                    element.find('.fc-title').prepend(event.description + "<br/>");
-                                },
-                                defaultView: 'month',
-                                lazyFetch: true,
-                                hiddenDays: [0],
-                                allDaySlot: false,
-                                minTime: '08:00:00',
-                                maxTime: '21:00:00',
-                                slotDuration: '00:15:00',
-                                contentHeight: 800,
-                                displayEventEnd: true
-                            });//fullCalendar
-                        });//ready
+                    $(document).ready(function() {
+                        $('#calendar').fullCalendar({
+                            eventSources: {
+                                url: "Include/feedEventosCentro.php?idCentro=<?php echo $idCentro; ?>"
+                            },
+                            header: {
+                                left: 'prev,today,next',
+                                center: 'title',
+                                right: 'agendaDay,agendaWeek,month'
+                            },
+                            eventRender: function(event, element) {
+                                element.find('.fc-title').prepend(event.description + "<br/>");
+                            },
+                            defaultView: 'month',
+                            lazyFetch: true,
+                            hiddenDays: [0],
+                            allDaySlot: false,
+                            minTime: '08:00:00',
+                            maxTime: '21:00:00',
+                            slotDuration: '00:15:00',
+                            contentHeight: 800,
+                            displayEventEnd: true,
+                            timeFormat: 'H:mm'
+                        });//fullCalendar
+                    });//ready
 </script>
