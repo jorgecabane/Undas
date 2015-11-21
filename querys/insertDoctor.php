cf94084473e40fd62bfc50bdc8d35bd8c43480cb
sikeze <mipozarski@alumnos.uai.cl>
2015-11-21 19:46:02 -0300

    doctorsitos arreglados :3

--- querys/insertDoctor.php
+++ querys/insertDoctor.php
@@ -5,8 +5,11 @@ $nombre = trim($_POST ['nombre']);
 $apellido = trim($_POST ['apellido']);
 	// comprobamos si ha ocurrido un error.
 	$rut=rand(0,9999090);
-	$query = "insert into tm values (null,'$nombre','$apellido','$rut',NULL,NULL,NULL,0, NULL,NULL,1,NULL,0)";
+	$query = "insert into tm values (null,'$nombre','$apellido','$rut',NULL,NULL,NULL,0, NULL,NULL,1,NULL,NULL,NULL)";
 	$resultado2 = mysql_query ( $query );
 	if ($resultado2) {
 		echo "Perfecto, redireccionando";
+	}
+	else {
+		echo ":o";
 	}
