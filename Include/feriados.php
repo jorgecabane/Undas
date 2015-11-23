b2384e3f44bcb3d429263f58f55ba6d6b6d26fce
Cesar Gonzalez <cgonzb@gmail.com>
2015-11-23 01:18:09 -0300

    se agregan feriados Diciembre

--- Include/feriados.php
+++ Include/feriados.php
@@ -29,6 +29,30 @@ $feriados = array(
         'end' => '2015-11-02',
         'description' => 'Dia de Todos los Santos',
         'feriado' => true
+    ),
+    array(
+        'start' => '2015-12-08T8:00:00',
+        'end' => '2015-12-08T21:00:00',
+        'description' => 'Asuncion de la Virgen',
+        'feriado' => true
+    ),
+    array(
+        'start' => '2015-12-08',
+        'end' => '2015-12-09',
+        'description' => 'Asuncion de la Virgen',
+        'feriado' => true
+    ),
+    array(
+        'start' => '2015-12-25T8:00:00',
+        'end' => '2015-12-25T21:00:00',
+        'description' => 'Natividad de Jesus',
+        'feriado' => true
+    ),
+    array(
+        'start' => '2015-12-25',
+        'end' => '2015-12-26',
+        'description' => 'Natividad de Jesus',
+        'feriado' => true
     )
 );
 echo json_encode($feriados);
