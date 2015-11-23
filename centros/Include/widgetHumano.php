97edb2187afe508c6c2a7ce542ff666e94a30722
jorgecabane93 <jorgecabane93@gmail.com>
2015-11-23 03:15:12 -0300

    widget prestaciones en logIn Empresas

--- centros/Include/widgetHumano.php
+++ centros/Include/widgetHumano.php
@@ -2,65 +2,166 @@
 
     <meta charset="utf-8">
 
-    
+
       <!-- <script type="text/javascript" src="maphilight-master/jquery.maphilight.js"></script> -->
 
-<div id="#map" style="position:relative; width:634px; height:607px; z-index:0; background-image:url(images/humano.jpg)">
+    <div id="map" style="position:relative; width:600px; height:800px; z-index:0; background-image:url(images/prestaciones.png); background-repeat: no-repeat;">
 
-<a class="popovereable" id="well map-link-1" href="#" shape="circle" 
-title="Mano" data-toggle="popover" data-trigger="focus" data-placement="auto"
-style="position:absolute;top:105px;left:540px;width:50px;height:50px; border: 2px solid red;border-radius: 50px;">&nbsp;</a>
+        <a class="popovereable" id="map-link-1" href="#" shape="square"
+           title="Doppler color Carotideo y Vertebral" data-toggle="popover" data-trigger="focus" data-placement="auto"
+           style="position:absolute;top:103px;left:22px;width:185px;height:53px; border: 1px solid #2F2F2F;border-radius: 2px;">
+        </a>
 
-<a class="popovereable" id="well map-link-2" href="#" shape="circle" 
-title="Pelvis Masculina" data-toggle="popover" data-trigger="focus" data-placement="auto"
-style="position:absolute;top:290px;left:300px;width:50px;height:50px; border: 2px solid red;border-radius: 50px;">&nbsp;</a>
-  <!--  <area shape="circle" coords="580,127,30" alt="Mano" href="#" title="Prestaciones Mano" data-toggle="popover" data-trigger="focus" data-placement="auto" >
-   <area shape="circle" coords="338,431,30" alt="Rodilla" href="#" title="Prestaciones Rodilla" data-toggle="popover" data-trigger="focus" data-placement="auto" >
-   <area shape="circle" coords="321,321,30" alt="Pelvis" href="#" title="Prestaciones Pelvis" data-toggle="popover" data-trigger="focus" data-placement="auto" >
-   <area shape="circle" coords="359,568,30" alt="Pie" href="#" title="Prestaciones Pie" data-toggle="popover" data-trigger="focus" data-placement="auto" >
-   <area shape="circle" coords="177,138,30" alt="Codo" href="#" title="Prestaciones Codo" data-toggle="popover" data-trigger="focus" data-placement="auto" >
-   <area shape="circle" coords="319,242,40" alt="Abdominal" href="#" title="Prestaciones Abdominal" data-toggle="popover" data-trigger="focus" data-placement="auto" >
-   <area shape="circle" coords="351,162,30" alt="Mamaria" href="#"  title="Prestaciones Mamaria" data-toggle="popover" data-trigger="focus" data-placement="auto" >
-   <area shape="circle" coords="317,112,30" alt="Cervical" href="#" title="Prestaciones Cervical" data-toggle="popover" data-trigger="focus" data-placement="auto" >
-   <area shape="circle" coords="263,120,30" alt="Hombro" href="#" title="Prestaciones Hombro" data-toggle="popover" data-trigger="focus" data-placement="auto" >
-   <area shape="circle" coords="499,133,30" alt="Antebrazo" href="#" title="Prestaciones Antebrazo" data-toggle="popover" data-trigger="focus" data-placement="auto" >
-   <area shape="circle" coords="334,504,30" alt="Pierna" href="#" title="Prestaciones Pierna" data-toggle="popover" data-trigger="focus" data-placement="auto" >
-   <area shape="circle" coords="289,363,30" alt="Muslo" href="#" title="Prestaciones Muslo" data-toggle="popover" data-trigger="focus" data-placement="auto">
-   <area shape="circle" coords="489,260,30" alt="Extras" href="#" title="Prestaciones Extras" data-toggle="popover" data-trigger="focus" data-placement="auto" >
- -->
-</div>
+        <a class="popovereable" id="map-link-2" href="#" shape="square"
+           title="Cervical" data-toggle="popover" data-trigger="focus" data-placement="auto"
+           style="position:absolute;top:39px;left:152px;width:91px;height:35px; border: 1px solid #2F2F2F;border-radius: 2px;">
+        </a>
 
-<script>
+        <a class="popovereable" id="map-link-3" href="#" shape="square"
+           title="Ecotomografía de Cerebro" data-toggle="popover" data-trigger="focus" data-placement="auto"
+           style="position:absolute;top:9px;left:253px;width:90px;height:30px; border: 1px solid #2F2F2F;border-radius: 2px;">
+        </a>
 
-	$(".popovereable").each(function(){
-		var especifico = $(this).attr('title');
-		//var idEmpresa = aqui va la id de la empresa (si es que se mete un centro);
-		var contenido = $(this);
-		var nombreEmpresa = $('.NombreEmpresa').text();
-	
-		    $.ajax({
-	    	method: "POST",
-	        url: "querys/getPrestacionesWidget.php",
-	        data: { 
-		           'especifico': especifico,
-		           'Empresa':  nombreEmpresa
-	              },
-	        success: function(response){
-	            contenido.attr("data-content",response);
-	       							   }
-	    });
-	    
-	
+        <a class="popovereable" id="map-link-4" href="#" shape="square"
+           title="Tiroides" data-toggle="popover" data-trigger="focus" data-placement="auto"
+           style="position:absolute;top:39px;left:366px;width:91px;height:35px; border: 1px solid #2F2F2F;border-radius: 2px;">
+        </a>
 
-	});
+        <a class="popovereable" id="map-link-5" href="#" shape="square"
+           title="Mamaria y Axilar" data-toggle="popover" data-trigger="focus" data-placement="auto"
+           style="position:absolute;top:120px;left:369px;width:174px;height:35px; border: 1px solid #2F2F2F;border-radius: 2px;">
+        </a>
 
-</script>
-<script>
-$(".popovereable").popover({
-	
-    html: true,
-    animation: true,
-    trigger: 'hover',
-});//popover
-</script>
+        <a class="popovereable" id="map-link-6" href="#" shape="square"
+           title="Doppler color Venoso de EESS" data-toggle="popover" data-trigger="focus" data-placement="auto"
+           style="position:absolute;top:186px;left:399px;width:174px;height:59px; border: 1px solid #2F2F2F;border-radius: 2px;">
+        </a>
+
+        <a class="popovereable" id="map-link-7" href="#" shape="square"
+           title="Doppler color Renal" data-toggle="popover" data-trigger="focus" data-placement="auto"
+           style="position:absolute;top:293px;left:446px;width:147px;height:34px; border: 1px solid #2F2F2F;border-radius: 2px;">
+        </a>
+
+        <a class="popovereable" id="map-link-8" href="#" shape="square"
+           title="Mano" data-toggle="popover" data-trigger="focus" data-placement="auto"
+           style="position:absolute;top:367px;left:487px;width:69px;height:29px; border: 1px solid #2F2F2F;border-radius: 2px;">
+        </a>
+
+        <a class="popovereable" id="map-link-9" href="#" shape="square"
+           title="Testicular" data-toggle="popover" data-trigger="focus" data-placement="auto"
+           style="position:absolute;top:473px;left:401px;width:102px;height:29px; border: 1px solid #2F2F2F;border-radius: 2px;">
+        </a>
+
+        <a class="popovereable" id="map-link-10" href="#" shape="square"
+           title="Doppler color Testicular" data-toggle="popover" data-trigger="focus" data-placement="auto"
+           style="position:absolute;top:511px;left:401px;width:186px;height:29px; border: 1px solid #2F2F2F;border-radius: 2px;">
+        </a>
+
+        <a class="popovereable" id="map-link-11" href="#" shape="square"
+           title="Doppler color Venoso de EEII" data-toggle="popover" data-trigger="focus" data-placement="auto"
+           style="position:absolute;top:583px;left:419px;width:165px;height:54px; border: 1px solid #2F2F2F;border-radius: 2px;">
+        </a>
+
+        <a class="popovereable" id="map-link-12" href="#" shape="square"
+           title="Pelvis Masculina" data-toggle="popover" data-trigger="focus" data-placement="auto"
+           style="position:absolute;top:600px;left:254px;width:107px;height:28px; border: 1px solid #2F2F2F;border-radius: 2px;">
+        </a>
+
+        <a class="popovereable" id="map-link-13" href="#" shape="square"
+           title="Pelvis Femenina" data-toggle="popover" data-trigger="focus" data-placement="auto"
+           style="position:absolute;top:628px;left:254px;width:107px;height:28px; border: 1px solid #2F2F2F;border-radius: 2px;">
+        </a>
+
+        <a class="popovereable" id="map-link-14" href="#" shape="square"
+           title="Doppler color Arterial de EEII" data-toggle="popover" data-trigger="focus" data-placement="auto"
+           style="position:absolute;top:588px;left:29px;width:164px;height:55px; border: 1px solid #2F2F2F;border-radius: 2px;">
+        </a>
+
+        <a class="popovereable" id="map-link-15" href="#" shape="square"
+           title="Inguinal" data-toggle="popover" data-trigger="focus" data-placement="auto"
+           style="position:absolute;top:492px;left:98px;width:84px;height:29px; border: 1px solid #2F2F2F;border-radius: 2px;">
+        </a>
+
+        <a class="popovereable" id="map-link-16" href="#" shape="square"
+           title="Doppler color Hepático" data-toggle="popover" data-trigger="focus" data-placement="auto"
+           style="position:absolute;top:410px;left:23px;width:95px;height:56px; border: 1px solid #2F2F2F;border-radius: 2px;">
+        </a>
+
+        <a class="popovereable" id="map-link-17" href="#" shape="square"
+           title="Renal" data-toggle="popover" data-trigger="focus" data-placement="auto"
+           style="position:absolute;top:348px;left:50px;width:61px;height:27px; border: 1px solid #2F2F2F;border-radius: 2px;">
+        </a>
+
+        <a class="popovereable" id="map-link-18" href="#" shape="square"
+           title="Abdomen" data-toggle="popover" data-trigger="focus" data-placement="auto"
+           style="position:absolute;top:293px;left:29px;width:102px;height:27px; border: 1px solid #2F2F2F;border-radius: 2px;">
+        </a>
+
+        <a class="popovereable" id="map-link-19" href="#" shape="square"
+           title="Doppler color Arterial de EESS" data-toggle="popover" data-trigger="focus" data-placement="auto"
+           style="position:absolute;top:196px;left:23px;width:164px;height:53px; border: 1px solid #2F2F2F;border-radius: 2px;">
+        </a>
+
+        <a class="popovereable" id="map-link-20" href="#" shape="square"
+           title="Ecotomografias niños de 0 a 5 años" data-toggle="popover" data-trigger="focus" data-placement="auto"
+           style="position:absolute;top:764px;left:111px;width:107px;height:30px; border: 1px solid #2F2F2F;border-radius: 2px;">
+        </a>
+
+        <a class="popovereable" id="map-link-21" href="#" shape="square"
+           title="Ecotomografias niños de 5 a 10 años" data-toggle="popover" data-trigger="focus" data-placement="auto"
+           style="position:absolute;top:764px;left:249px;width:120px;height:30px; border: 1px solid #2F2F2F;border-radius: 2px;">
+        </a>
+
+        <a class="popovereable" id="map-link-22" href="#" shape="square"
+           title="Ecotomografias niños  sobre 10 años" data-toggle="popover" data-trigger="focus" data-placement="auto"
+           style="position:absolute;top:764px;left:400px;width:160px;height:30px; border: 1px solid #2F2F2F;border-radius: 2px;">
+        </a>
+        <!--<area shape="circle" coords="580,127" alt="Mano" href="#" title="Prestaciones Mano" data-toggle="popover" data-trigger="focus" data-placement="auto" >
+           <area shape="circle" coords="338,431,30" alt="Rodilla" href="#" title="Prestaciones Rodilla" data-toggle="popover" data-trigger="focus" data-placement="auto" >
+          <area shape="circle" coords="321,321,30" alt="Pelvis" href="#" title="Prestaciones Pelvis" data-toggle="popover" data-trigger="focus" data-placement="auto" >
+          <area shape="circle" coords="359,568,30" alt="Pie" href="#" title="Prestaciones Pie" data-toggle="popover" data-trigger="focus" data-placement="auto" >
+          <area shape="circle" coords="177,138,30" alt="Codo" href="#" title="Prestaciones Codo" data-toggle="popover" data-trigger="focus" data-placement="auto" >
+          <area shape="circle" coords="319,242,40" alt="Abdominal" href="#" title="Prestaciones Abdominal" data-toggle="popover" data-trigger="focus" data-placement="auto" >
+          <area shape="circle" coords="351,162,30" alt="Mamaria" href="#"  title="Prestaciones Mamaria" data-toggle="popover" data-trigger="focus" data-placement="auto" >
+          <area shape="circle" coords="317,112,30" alt="Cervical" href="#" title="Prestaciones Cervical" data-toggle="popover" data-trigger="focus" data-placement="auto" >
+          <area shape="circle" coords="263,120,30" alt="Hombro" href="#" title="Prestaciones Hombro" data-toggle="popover" data-trigger="focus" data-placement="auto" >
+          <area shape="circle" coords="499,133,30" alt="Antebrazo" href="#" title="Prestaciones Antebrazo" data-toggle="popover" data-trigger="focus" data-placement="auto" >
+          <area shape="circle" coords="334,504,30" alt="Pierna" href="#" title="Prestaciones Pierna" data-toggle="popover" data-trigger="focus" data-placement="auto" >
+          <area shape="circle" coords="289,363,30" alt="Muslo" href="#" title="Prestaciones Muslo" data-toggle="popover" data-trigger="focus" data-placement="auto">
+          <area shape="circle" coords="489,260,30" alt="Extras" href="#" title="Prestaciones Extras" data-toggle="popover" data-trigger="focus" data-placement="auto" >
+        -->
+    </div>
+
+    <script>
+
+        $(".popovereable").each(function() {
+            var especifico = $(this).attr('title');
+            //var idEmpresa = aqui va la id de la empresa (si es que se mete un centro);
+            var contenido = $(this);
+
+
+            $.ajax({
+                method: "POST",
+                url: "querys/getPrestacionesWidget.php",
+                data: {
+                    'especifico': especifico
+                            // 'idEmpresa': idEmpresa
+                },
+                success: function(response) {
+                    contenido.attr("data-content", response);
+                }
+            });
+
+
+
+        });
+
+    </script>
+    <script>
+        $(".popovereable").popover({
+            html: true,
+            animation: true,
+            trigger: 'hover'
+        });//popover
+    </script>
 </html>
