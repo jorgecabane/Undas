8a7a20abec6126f5620b15c9842c75a74f4d91e5
Cabané <jorgecabane93@gmail.com>
2015-12-10 02:24:06 -0300

    arreglo para get bugs admin

--- admin/Include/bugs.php
+++ admin/Include/bugs.php
@@ -1,6 +1,7 @@
 <?php
 include_once dirname(dirname(__FILE__)) . '/querys/getBugs.php';//conexion local
-include_once dirname(dirname(__FILE__)) . '/querys/getTM.php';//geTM
+include_once dirname(dirname(__FILE__)) . '/querys/getTM.php';//getTM
+include_once dirname(dirname(__FILE__)) . '/querys/getEmpresa.php';//getEmpresa
 echo "<table class='table table-condensed table-bordered table-hover'>
         <thead>
             <tr>
@@ -15,21 +16,29 @@ $bugs = getBugs();
 if ($bugs) {
     foreach($bugs as $bug){
     	echo "<tr>";
-
+if($bug['tm_idTM']!=NULL){
  $TM= getTecnologo($bug['tm_idTM']);
  foreach($TM as $dato)
  {
- 	  	  echo	"<td>$dato[Nombre]";
- 	  	 echo " ";
- 	  	 echo "$dato[Apellido]</td>";
+ 	  	 echo  "<td>$dato[Nombre]";
+ 	  	 echo  " ";
+ 	  	 echo  "$dato[Apellido]</td>";
  }
-       
+} // en caso de que row sea de un Tm
+else{
+	$Empresa= getEmpresaAdmin($bug['empresa_idEmpresa']);
+	foreach($Empresa as $datoEmpresa)
+	{
+		echo  "<td>$datoEmpresa[Nombre]";
+	}
+} //en caso de que row sea de una empresa
           echo "<td>$bug[Titulo]</td>
                 <td>$bug[Descripcion]</td>
                 <td>$bug[fecha]</td>
              </tr>";
-    }
-}
+    } // for each de los bugs
+
+    } // Si es que hay Bugs
 echo "  </tbody>
       </table>  ";
 ?>
