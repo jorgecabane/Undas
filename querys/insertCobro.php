e3fe7a4ec6cccb45266c0ada28078b44cf6f4105
Cabané <jorgecabane93@gmail.com>
2015-12-09 23:44:32 -0300

    1er arreglo querys

--- querys/insertCobro.php
+++ querys/insertCobro.php
@@ -1,26 +1,18 @@
 
 <?php
 include_once "../conexionLocal.php";
-$idTM=trim($_POST['idTM']);
+$rutTM=trim($_POST['idTM']);
 $valor=trim($_POST['cobro']);
 $empresa=trim($_POST['idEmpresa']);
 $semana=trim($_POST['semana']);
 
-$queryIdTM="Select idTM from tm where Rut='$idTM'";
+$queryIdTM="Select idTM from tm where Rut='$rutTM'";
 $resultado= mysql_query($queryIdTM);
 $idassoc= mysql_fetch_assoc($resultado);
 $idtecnologo=$idassoc['idTM'];
 
-
-
-$queryrut="Select Rut as RutEmpresa from empresa where idEmpresa=$empresa";
-$resultadorut= mysql_query($queryrut);
-$Assoc= mysql_fetch_assoc($resultadorut);
-$rutempresa=$Assoc['RutEmpresa'];
-
-
 	// comprobamos si ha ocurrido un error.
-	$query = "insert into valorhora values (null,$idtecnologo,$valor,$semana,$empresa,'$rutempresa')";
+	$query = "insert into valorhora values (null,$idtecnologo,$valor,$semana,$empresa)";
 
 	
 	$resultado2 = mysql_query ( $query );
