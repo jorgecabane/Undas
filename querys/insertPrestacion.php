bd9d52d1b23d9a8bf7d691b4a2d63065abadd54b
sikeze <mipozarski@alumnos.uai.cl>
2015-12-09 23:57:00 -0300

    arreglos que hizo "jorge" choripan

--- querys/insertPrestacion.php
+++ querys/insertPrestacion.php
@@ -2,7 +2,7 @@
 <?php
 include_once "../conexionLocal.php";
 $idTM=$_POST['idTM'];
-$empresa=$_POST['idEmpresa'];
+$empresa=trim($_POST['idEmpresa']);
 $idPrestacion=$_POST['idPrestacion'];
 
 $queryIdTM="Select idTM from tm where Rut='$idTM'";
@@ -10,16 +10,8 @@ $resultado= mysql_query($queryIdTM);
 $idassoc= mysql_fetch_assoc($resultado);
 $idtecnologo=$idassoc['idTM'];
 
-
-
-$queryrut="Select Rut as RutEmpresa from empresa where idEmpresa=$empresa";
-$resultadorut= mysql_query($queryrut);
-$Assoc= mysql_fetch_assoc($resultadorut);
-$rutempresa=$Assoc['RutEmpresa'];
-
-
 	// comprobamos si ha ocurrido un error.
-	$query = "insert into prestacionestm values ($idPrestacion,$idtecnologo,'$idTM',$empresa,'$rutempresa')";
+	$query = "insert into prestacionestm values ($idPrestacion,$idtecnologo,$empresa)";
 
 	
 	$resultado2 = mysql_query ( $query );
