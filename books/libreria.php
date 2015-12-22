c95ac374576ce3421beca3b3a69854496cd32d58
jorgecabane93 <jorgecabane93@gmail.com>
2015-12-22 19:14:19 -0300

    arreglines para server

--- books/libreria.php
+++ books/libreria.php
@@ -24,7 +24,7 @@ include_once dirname(dirname(__FILE__)) . "/Include/isAdmin.php";
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <![endif]-->
 <meta charset="utf-8">
-<title>jQuery File Upload- jQuery UI version</title>
+<title>TMTecnomed Storage's</title>
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <!-- jQuery UI styles -->
