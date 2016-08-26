27cea9f7159878139762a73566a2952dd4bff950
cgonzb <cgonzb@gmail.com>
2016-08-25 22:50:16 -0300

    evento renderea con idtm

--- Include/js/renderEvent.js
+++ Include/js/renderEvent.js
@@ -1,15 +1,17 @@
 /*
- * @param {type} : event
- * @param {type} : element
- * @returns {setea el popover para eventos}
- */
-var renderEvent = function(event, element) {
+ * @param obj event
+ * @param obj element
+ * @return {setea el popover para eventos}
+ **/
+var renderEvent = function (event, element) {
+    //console.log(element);
     if (event.start.hasTime()) {//si el evento tiene hora asignada
         //se agrega la descripcion al evento
         element.find('.fc-title').append("<br/>" + event.description);
+        element.attr("idTM", event.idTM);
         //al hacer click se puede ver el detalle
         element.popover({
-            title: 'Detalles del Evento (' + event.id +')',
+            title: 'Detalles del Evento (' + event.id + ')',
             content: '<div><b>Eco: </b>' + event.title + '<br>\n\
                              <b>TM: </b>' + event.description + '<br>\n\
                              <b>Fecha: </b>' + event.start.format('LL') + '<br>\n\
